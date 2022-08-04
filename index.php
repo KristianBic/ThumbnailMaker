595c852213e76fa5ee3215b58c636b434dea8804
KristianBic <92215832+KristianBic@users.noreply.github.com>
2022-08-04 14:52:23 +0200

    INIT

---
+++ index.php
@@ -0,0 +1,49 @@
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="style/style.css" />
+	<link rel="stylesheet" href="style/style_home.css" />
+	<link rel="icon" type="image/x-icon" href="../images/favicon.svg" />
+	<script defer src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script defer src="library/html2canvas.min.js"></script>
+	<script defer src="script/index.js"></script>
+	<title id="Title">Maker</title>
+</head>
+
+<body>
+	<?php include 'pages/header.php' ?>
+	<div class="content">
+		<div class="left-navbar">
+			<ul class="sidebar">
+				<li><img src="assets/icons/nav_arrow.svg" alt=""></li>
+				<li><img src="assets/icons/nav_text.svg" alt=""></li>
+				<li><img src="assets/icons/nav_color.svg" alt=""></li>
+				<li><img src="assets/icons/nav_image.svg" alt=""></li>
+			</ul>
+		</div>
+		<div class="sidebar-content">
+			<h2>TEXT OPTIONS</h2>
+			<div id="text_inputs">
+				<p data-text="Text line 1">Text Line 1</p>
+				<input class="text-Input" type="text" placeholder="Enter Text" data-text="Text line 1" oninput="changeText()">
+				<p data-text="Text line 2">Text Line 2</p>
+				<input class="text-Input" type="text" placeholder="Enter Text" data-text="Text line 2" oninput="changeText()">
+			</div>
+			<button onclick="addNewTextLine()"><img src="assets/icons/addLine.svg" alt="">Add Text Line</button>
+		</div>
+		<div class="canva-container">
+			<h6>Canva Preview</h6>
+			<div class="canva" id="canvas_id">
+				<p data-movable="true" data-text="Text line 1" class="text-canva">Text 1</p>
+				<p data-movable="true" data-text="Text line 2" class="text-canva">Text 2</p>
+			</div>
+			<button id="btn_Create_Image">Create Image</button>
+			<a id="download">Download</a>
+		</div>
+	</div>
+</body>
+
+</html>
---
+++ script/index.js
@@ -0,0 +1,84 @@
+let currentTextNumber = 2;
+
+
+$(".text-Input").on('input', event => {
+    changeText(event);
+})
+
+
+function changeText(event) {
+    const currentInput = event.target;
+    const inputData = currentInput.dataset.text;
+    const currentTextOnCanvas = $("p[data-text='" + inputData + "']");
+
+    currentTextOnCanvas.text(currentInput.value);
+}
+
+function addNewTextLine() {
+    currentTextNumber++;
+
+    const inputLabel = document.createElement('p');
+    const input = document.createElement('input');
+    const textOnCanvas = document.createElement('p');
+
+    input.setAttribute('class', 'text-Input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', 'Enter Text');
+    input.setAttribute('data-text', `Text line ${currentTextNumber}`);
+    textOnCanvas.setAttribute('data-text', `Text line ${currentTextNumber}`);
+    textOnCanvas.setAttribute('data-movable', `true`);
+    textOnCanvas.setAttribute('class', 'text-canva');
+
+    inputLabel.innerHTML = input.dataset.text;
+    textOnCanvas.innerHTML = `Text ${currentTextNumber}`;
+
+    input.addEventListener('input', changeText);
+    textOnCanvas.addEventListener('mousedown', movingElements);
+
+    $("#text_inputs").append(inputLabel);
+    $("#text_inputs").append(input);
+    $('.canva').append(textOnCanvas);
+}
+
+$('#btn_Create_Image').on("click", () => {
+    const can = document.getElementById('canvas_id');
+    html2canvas(can).then(canvas => {
+        const image = canvas.toDataURL("image/jpg");
+        const newData = image.replace(/^data:image\/jpg/, "data:application/octet-stream");
+        $("#download").attr("download", "image.jpg").attr("href", newData);
+    });
+});
+
+
+$('*[data-movable="true"]').on("mousedown", (event) => {
+    movingElements(event);
+});
+
+
+function onDrag({
+    movementX,
+    movementY,
+    target
+}) {
+    let getStyle = window.getComputedStyle(target);
+    let leftVal = parseInt(getStyle.left);
+    let topVal = parseInt(getStyle.top);
+
+    target.style.left = `${leftVal + movementX}px`;
+    target.style.top = `${topVal + movementY}px`;
+
+}
+
+function movingElements(event) {
+    const element = event.target;
+    element.style.position = "absolute";
+    element.classList.add("active");
+    element.addEventListener("mousemove", onDrag);
+
+    //instead of document, we can use 'element', however there is a bug where when we "mouseUp" outside the 'element', listener mouseup doesn't process
+    document.addEventListener("mouseup", function mouseUpList() {
+        element.classList.remove("active");
+        element.removeEventListener("mousemove", onDrag);
+        document.removeEventListener("mouseup", mouseUpList);
+    });
+};
